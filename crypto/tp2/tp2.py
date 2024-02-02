baeaf4f5409d906cc020e462f518859dfdacfc90
Siwa12100 <jean.marcillac12@gmail.com>
2024-02-02 14:21:27 +0100

    tp 2 python

--- crypto/tp2/tp2.py
+++ crypto/tp2/tp2.py
@@ -2,32 +2,25 @@ import math
 import binascii
 
 
-# def chiffrementParfait(message, cle) :
+def chiffrementParfait(message, cle) :
 
-#     msgCrypte = "coucou"
-
-#     msgCrypte = msgCrypte + "test"
-
-#     print(msgCrypte)
-#     print(ord('a'))
-
-#     return msgCrypte
 
 
 def stringToBinaire(message) :
-    resultat = ""
+    resultat = []
     
-    for i in message :
-        resultat = resultat + (chr(int(i, 2)))
+    for i in range (len(message)) :
+        resultat.append(bin(ord(message[i])))
 
     return resultat
 
 
 
 def binaireToString(message) :
+    
     resultat = ""
-    for i in range (len(message)) : 
-        resultat.append(bin(ord(message[i])))
+    for i in message : 
+        resultat = resultat + (chr(int(i,2)))
 
     return resultat
 
