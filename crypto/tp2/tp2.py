928dbcc7d2a6c4cc78887a70302b523a996b147c
Siwa12100 <jean.marcillac12@gmail.com>
2024-02-02 15:21:29 +0100

    travail sur le tp2 de crypto

--- crypto/tp2/tp2.py
+++ crypto/tp2/tp2.py
@@ -2,15 +2,12 @@ import math
 import binascii
 
 
-def chiffrementParfait(message, cle) :
-
-
-
 def stringToBinaire(message) :
     resultat = []
     
     for i in range (len(message)) :
         resultat.append(bin(ord(message[i])))
+        # print("Xor : ", bin(ord(message[i])) ^ bin(ord(message[i])))
 
     return resultat
 
@@ -19,14 +16,84 @@ def stringToBinaire(message) :
 def binaireToString(message) :
     
     resultat = ""
+
     for i in message : 
         resultat = resultat + (chr(int(i,2)))
 
     return resultat
 
-print(stringToBinaire("coucou"))
 
-print(binaireToString(stringToBinaire("coucou")))
+# print(stringToBinaire("coucou"))
+#print(binaireToString(stringToBinaire("coucou")))
 
 
-# chiffrementParfait("coucou", "huit")
+# def chiffrementParfait(message, cle) :
+
+    resultat = 0
+    cleBinaire = stringToBinaire(cle)
+    messageBinaire = stringToBinaire(message)
+
+    print("cle binaire : ", cleBinaire)
+    print("message binaire : ", messageBinaire)
+    #print("type : ", messageBinaire.type())
+
+    print()
+    print("================")
+    print("conversion test : ", binaireToString(messageBinaire))
+    print("================")
+    print()
+
+
+    if (len(message) != len(cle)) :
+        return "erreur"
+
+    # for i in range (len(messageBinaire)) :
+    #     print("messageBinaire[i] :", int(messageBinaire[i][2:]))
+    #     print("cleBinaire[i] :", int(cleBinaire[i][2:]))
+
+    #     resultat = resultat + (int(messageBinaire[i][2:]) ^ int(cleBinaire[i][2:]))
+
+    for i in range (len(messageBinaire ) - 2) :
+        for i in range (int(messageBinaire[i][2:])) :
+            print("I  ---> ", i)
+        
+    return resultat
+
+
+def chiffrementParfait(message, cle) :
+    resultat = ""
+
+    for i in range (len(message)) :
+
+        lettreBinaireMessage = stringToBinaire(message[i])
+        lettreBinaireCle = stringToBinaire(cle[i])
+
+
+        print("taille : ", len(lettreBinaireMessage))
+        print("lettreBinaireMessage : ", lettreBinaireMessage[0][2:])
+
+        valeurTraiteeMessage = lettreBinaireMessage[0][2:]
+        valeurTraiteeCle = lettreBinaireCle[0][2:]
+
+        print("taille valeurTraitee : ", len(valeurTraiteeMessage))
+
+        xor = int(valeurTraiteeMessage[0]) ^ int(valeurTraiteeCle[0])
+
+        for b in range(1, len(valeurTraiteeMessage)) :
+            # print("M --> ", valeurTraiteeMessage[b])
+            # print("C --> ", valeurTraiteeCle[b])
+
+            # print("Xor ... : " , int(valeurTraiteeMessage[b]) ^ int(valeurTraiteeCle[b]))
+            xor = xor + int(valeurTraiteeMessage[b]) ^ int(valeurTraiteeCle[b])
+
+        print("Xor final : ", xor)
+
+    return resultat    
+
+print(chiffrementParfait("coucou", "voitur"))
+
+
+
+
+
+
