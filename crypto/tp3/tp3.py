447a683ca1a4e9ac75e1221f5239cfa40965cf32
Siwa12100 <jean.marcillac12@gmail.com>
2024-02-09 13:56:02 +0100

    taf sur la crypto

--- crypto/tp3/tp3.py
+++ crypto/tp3/tp3.py
@@ -1,23 +1,11 @@
 import sympy
 import random
 
-# ============
-# Exercice 1 :
-# ============
-
-# print("debut du tp3.")
-
-import sympy
-import random
-
 def Clefs(k):
-
     p = sympy.randprime(10**k, 10**(k+1) - 1)
     g = random.randint(2, p - 1)
     s = random.randint(2, p - 2)
-
     h = pow(g, s, p)
-
     return (p, g, h), s
 
 k = 10
@@ -25,5 +13,13 @@ clefs_publique, clef_privee = Clefs(k)
 
 print("Clef publique (p, g, h):", clefs_publique)
 print("Clef privée (s):", clef_privee)
+
+s_secret = random.randint(2, clefs_publique[0] - 1)
+
+h_secret = pow(clefs_publique[1], s_secret, clefs_publique[0])
+
+print("Nombre secret (s):", s_secret)
+print("h calculé avec le secret (g^s mod p):", h_secret)
+
 
 
