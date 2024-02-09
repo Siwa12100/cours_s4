e770f49fb33179b2ca1e6bd0e4fefb6a496becbe
Siwa12100 <jean.marcillac12@gmail.com>
2024-02-09 13:54:11 +0100

    taf sur la crypto

--- crypto/tp3/tp3.py
+++ crypto/tp3/tp3.py
@@ -1,3 +1,6 @@
+import sympy
+import random
+
 # ============
 # Exercice 1 :
 # ============
@@ -8,27 +11,19 @@ import sympy
 import random
 
 def Clefs(k):
-    # Fonction pour générer une paire de clefs El Gamal
 
-    # Étape 1 : Choix du module p
     p = sympy.randprime(10**k, 10**(k+1) - 1)
-
-    # Étape 2 : Choix du générateur g
     g = random.randint(2, p - 1)
+    s = random.randint(2, p - 2)
 
-    # Choix de la clef privée s
-    s = random.randint(2, p - 2)  # s doit être dans l'intervalle [2, p-2]
-
-    # Calcul de h = g^s mod p
     h = pow(g, s, p)
 
-    # Retourner la paire de clefs publique et privée
     return (p, g, h), s
 
-# Exemple d'utilisation avec k=10
 k = 10
 clefs_publique, clef_privee = Clefs(k)
 
-# Affichage des résultats
 print("Clef publique (p, g, h):", clefs_publique)
 print("Clef privée (s):", clef_privee)
+
+
