7b44db0c7893758d77376e5d04f4cd3b3650af48
Siwa12100 <jean.marcillac12@gmail.com>
2024-02-23 13:48:52 +0100

    taff sur le tp 5 de crypto

--- crypto/tp5/tp.py
+++ crypto/tp5/tp.py
@@ -1,6 +1,9 @@
 # ============
 # Exercice 1 :
 # ============
+print()
+print("------ Exo 1 -------")
+
 
 def decode_ascii(binary_string):
     bytes_liste = [binary_string[i:i+8] for i in range(0, len(binary_string), 8)]
@@ -24,6 +27,40 @@ message = "01000011011011110110110101101101011001010110111001100011" \
                   "01101111011011100111001100101110"
 
 message_decode = decode_ascii(message)
-print(message_decode)
+print("Message decodé  : ", message_decode)
+
+
+# ============
+# Exercice 2 :
+# ============
+print()
+print(" ------- Exo 2 -------")
+
+def extraire_bits_information(bits_encodes):
+    bits_information = ""
+    for i in range(0, len(bits_encodes), 7):
+        bits_information += bits_encodes[i:i+4]
+    return bits_information
+
+
+def decodage_hamming(bits_encodes):
+    bits_corriges = ""
+    for i in range(0, len(bits_encodes), 7):
+        bloc = bits_encodes[i:i+7]
+        controle_parite = [int(bloc[4]) ^ int(bloc[0]) ^ int(bloc[1]) ^ int(bloc[2]),
+                            int(bloc[5]) ^ int(bloc[0]) ^ int(bloc[1]) ^ int(bloc[3]),
+                            int(bloc[6]) ^ int(bloc[1]) ^ int(bloc[2]) ^ int(bloc[3])]
+
+        position_erreur = sum([2**i for i, bit in enumerate(controle_parite) if bit == 1])
+
+        if position_erreur != 0:
+            bloc_corrigé = list(bloc)
+            bloc_corrigé[position_erreur - 1] = str(1 - int(bloc_corrigé[position_erreur - 1]))
+            bits_corriges += "".join(bloc_corrigé)[:4]
+        else:
+            bits_corriges += bloc[:4]
+
+    return bits_corriges
+
 
 
