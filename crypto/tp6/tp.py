c5055478e57e1f4834dc0924ffbcf3f4a75b22ce
Siwa12100 <jean.marcillac12@gmail.com>
2024-03-15 14:17:11 +0100

    taff tp de crypto

--- crypto/tp6/tp.py
+++ crypto/tp6/tp.py
@@ -65,7 +65,56 @@ def Verification(conn):
 print()
 print("--- Exo 2 : ---")
 
+import hashlib
+
+def hacher_mot_de_passe(mot_de_passe):
+    hachage = hashlib.sha256(mot_de_passe.encode()).hexdigest()
+    return hachage
+
+def AjoutUtilisateur2(conn):
+    login = input("Entrez son login : ")
+    password = input("Entrez son mot de passe : ")
+    password_confirm = input("Confirmez son mot de passe : ")
+    
+    if password != password_confirm:
+        print("Les mots de passe ne correspondent pas.")
+        return
+    
+    curseur = conn.cursor()
+    curseur.execute("SELECT * FROM utilisateurs WHERE name=?", (login,))
+    
+    if curseur.fetchone():
+        print("Ce login existe déjà.")
+        return
+    
+    password_hashe = hacher_mot_de_passe(password)
+    curseur.execute("INSERT INTO utilisateurs (name, password) VALUES (?, ?)", (login, password_hashe))
+    conn.commit()
+    print("Utilisateur ajouté avec succès.")
+
+
+def Verification2(conn):
+    login = input("Login : ")
+    password = input("Mot de passe : ")
+    password_hashe = hacher_mot_de_passe(password)
+    
+    curseur = conn.cursor()
+    curseur.execute("SELECT * FROM utilisateurs WHERE name=? AND password=?", (login, password_hashe))
+    
+    if curseur.fetchone():
+        print("Authentification réussie.")
+    else:
+        print("Échec de l'authentification.")
+
+
+# AfficherUtilisateurs(connection)
+# AjoutUtilisateur2(connection)
+# AfficherUtilisateurs(connection)
+# Verification2(connection)
+
 # ----------------
+
+
 connection.close()
 
 
